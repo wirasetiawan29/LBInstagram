9d12fb5185bea51b148d35e5decec979df19243b
wira <wirasetiawan29@gmail.com>
2019-08-24 16:30:23 +0700

    init

---
+++ LBInstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBInstagram
+//
+//  Created by wira on 8/24/19.
+//  Copyright © 2019 Wira Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
