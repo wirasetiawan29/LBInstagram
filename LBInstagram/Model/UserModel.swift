1d831c07691517fe0f6db35b9a1ae791f99e481a
wira <wirasetiawan29@gmail.com>
2019-08-24 18:23:28 +0700

    login flow

---
+++ LBInstagram/Model/UserModel.swift
@@ -0,0 +1,27 @@
+//
+//  UserModel.swift
+//  LBInstagram
+//
+//  Created by wira on 8/24/19.
+//  Copyright © 2019 Wira Setiawan. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+
+
+    var userId: Int64 = 0
+    var userLocalId: Int64 = Int64(Date.init().timeIntervalSince1970)
+    var userName: String  = ""
+    var userFirstName: String = ""
+    var userLastName: String = ""
+    var userGender: Bool? = nil
+    var userPhoneNumber: String = ""
+    var userProfileImage: String = ""
+
+    override init() {
+        super.init()
+    }
+
+}
